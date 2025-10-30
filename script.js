3bacd3a3a3bc3df7ed4bb6a15677d6d0b7300822
malif <maliffia121212121@gmail.com>
2025-10-30 11:14:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const tombolSapa = document.getElementByid('sapabutton');
 tombolSapa.addEventListener('click'function(){
-  alert('Halo!terimakasih sudah berkunjung
+  alert('Halo!terimakasih sudah berkunjung!);});
         
